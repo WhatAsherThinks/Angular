43771548b9eb3346e7e6142ed7910c9ea0036283
Asher <asher.demadet@gmail.com>
2019-08-21 20:09:18 -0400

    explores passing data with services

--- recipe-book/src/app/shopping-list/shipping-list.service.ts
+++ recipe-book/src/app/shopping-list/shipping-list.service.ts
@@ -1,3 +1,26 @@
-export class ShoppingListService {
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 7),
+    new Ingredient('flour', 2)
+  ];
+
+  getIngredients(){
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // ... is a spread operator that turns arrays into a list of seperate items
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
