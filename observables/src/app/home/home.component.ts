267111b87499cf073d9677304798ee9cced8b003
WhatAsherThinks <asher.demadet@gmail.com>
2019-08-27 19:12:43 -0400

    use of pipes

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -22,7 +23,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval( () => {
         observer.next(count);
-        if(count === 2){
+        if(count === 5) {
           observer.complete();
         }
         if(count > 3) {
@@ -32,7 +33,12 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+
+    this.firstObsSubscription =  customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
